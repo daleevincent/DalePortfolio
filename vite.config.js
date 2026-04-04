e24be27de3940be69b4b09dcf4916f26133e5031
daleevincent <you@example.com>
2026-04-04 16:16:33 +0800

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // change to '/' if not deploying to GitHub Pages subfolder
+  base: '/Main_Portfolio/', // change to '/' if not deploying to GitHub Pages subfolder
 })
